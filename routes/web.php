238ecdec138b614b98eb30d985d3e9ca9d292fde
equipocorp26 <equipocorp26@gmail.com>
2022-07-20 19:35:13 -0400

    Api Login/Logout

--- routes/web.php
+++ routes/web.php
@@ -5,37 +5,30 @@ use App\Http\Controllers\MovementController;
 use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+/* No Auth */
+Auth::routes();
+/* Auth */
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    /* BALANCES */
+    Route::resource('balances', BalanceController::class)->names('balances');
+    /* MOVEMENTS */
+    Route::get('balances/{balance}/movements',[MovementController::class,'index'])->name('movements.index');
+    Route::get('balances/{balance}/movements/create',[MovementController::class,'create'])->name('movements.create');
+    Route::post('balances/{balance}/movements',[MovementController::class,'store'])->name('movements.store');
+    Route::get('balances/{balance}/movements/{movement}',[MovementController::class,'show'])->name('movements.show');
+    Route::delete('balances/{balance}/movements/{movement}',[MovementController::class,'destroy'])->name('movements.destroy');
+    /* RESERVACIONES */
+    Route::get('balance/{balance}/reservations',[ReservationController::class,'index'])->name('reservations.index');
+    Route::get('balances/{balance}/reservations/create',[ReservationController::class,'create'])->name('reservations.create');
+    Route::post('balances/{balance}/reservations',[ReservationController::class,'store'])->name('reservations.store');
+    Route::get('balances/{balance}/reservations/{reservation}',[ReservationController::class,'show'])->name('reservations.show');
+    Route::delete('balances/{balance}/reservations/{reservation}',[ReservationController::class,'destroy'])->name('reservations.destroy');
+    Route::post('balances/{balance}/reservations/{reservation}',[ReservationController::class,'check'])->name('reservations.check');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-/* BALANCES */
-Route::resource('balances', BalanceController::class)->names('balances');
-/* MOVEMENTS */
-Route::get('balances/{balance}/movements',[MovementController::class,'index'])->name('movements.index');
-Route::get('balances/{balance}/movements/create',[MovementController::class,'create'])->name('movements.create');
-Route::post('balances/{balance}/movements',[MovementController::class,'store'])->name('movements.store');
-Route::get('balances/{balance}/movements/{movement}',[MovementController::class,'show'])->name('movements.show');
-Route::delete('balances/{balance}/movements/{movement}',[MovementController::class,'destroy'])->name('movements.destroy');
-/* RESERVACIONES */
-Route::get('balance/{balance}/reservations',[ReservationController::class,'index'])->name('reservations.index');
-Route::get('balances/{balance}/reservations/create',[ReservationController::class,'create'])->name('reservations.create');
-Route::post('balances/{balance}/reservations',[ReservationController::class,'store'])->name('reservations.store');
-Route::get('balances/{balance}/reservations/{reservation}',[ReservationController::class,'show'])->name('reservations.show');
-Route::delete('balances/{balance}/reservations/{reservation}',[ReservationController::class,'destroy'])->name('reservations.destroy');
-Route::post('balances/{balance}/reservations/{reservation}',[ReservationController::class,'check'])->name('reservations.check');
 
